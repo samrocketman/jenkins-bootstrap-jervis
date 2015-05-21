53fb67e40c97093a84c65ab23b6397ac39c541ac
Sam Gleske <sam.mxracer@gmail.com>
2015-05-21 01:09:06 -0400

    Add more comments to jervis_bootstrap.sh

--- jervis_bootstrap.sh
+++ jervis_bootstrap.sh
@@ -6,8 +6,9 @@
 #GNU bash, version 4.3.11(1)-release (x86_64-pc-linux-gnu)
 #curl 7.35.0 (x86_64-pc-linux-gnu) libcurl/7.35.0 OpenSSL/1.0.1f zlib/1.2.8 libidn/1.28 librtmp/2.3
 
-#A script which bootstraps a Jenkins installation using Jervis
+#A script which bootstraps a Jenkins installation for executing Jervis Job DSL scripts
 
+#grab the latest copy of the provision_jenkins.sh script
 curl -sLo provision_jenkins.sh 'https://raw.githubusercontent.com/samrocketman/home/master/bin/provision_jenkins.sh'
 chmod 755 provision_jenkins.sh
 #download jenkins, start it up, and update the plugins
@@ -16,5 +17,6 @@ chmod 755 provision_jenkins.sh
 ./provision_jenkins.sh install-plugins cloudbees-folder job-dsl view-job-filters embeddable-build-status
 #restart jenkins
 ./provision_jenkins.sh restart
+#create the first job, _jervis_generator.  This will use Job DSL scripts to generate other jobs.
 ./provision_jenkins.sh cli create-job _jervis_generator < jervis_config.xml
 echo 'Jenkins is ready.  Visit http://localhost:8080/'
