e9acd678d0b8d14ee06e219c0c6d456b5020fe11
Sam Gleske <sam.mxracer@gmail.com>
2015-05-21 00:28:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Bootstrap Jenkins using Jervis
+
+[Jervis][jervis] generates Jenkins jobs using Travis CI YAML.
+
+# Instructions
+
+### GitHub API key
+
+Log into GitHub and [generate an API token][gh-token] so that Jervis can
+authenticate to the GitHub API.
+
+### Provision Jenkins
+
+To provision Jenkins and create the seed job simply run the following command.
+Don't forget to export your GitHub API token to the `GITHUB_TOKEN` environment
+variable.
+
+    #not a real token
+    export GITHUB_TOKEN="abca2bf1000cd67f7d805612b43195ce9c10a123"
+    ./jervis_bootstrap.sh
+
+Visit http://localhost:8080/ to see Jenkins running with Jervis.  Build the
+`_jervis_generator` which will interact with the GitHub API to create Jenkins
+jobs based on Travis YAML.
+
+[jervis]: https://github.com/samrocketman/jervis
+[gh-token]: https://help.github.com/articles/creating-an-access-token-for-command-line-use/
---
+++ jervis_bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Created by Sam Gleske (https://github.com/samrocketman)
+#Wed May 20 23:22:07 EDT 2015
+#Ubuntu 14.04.2 LTS
+#Linux 3.13.0-52-generic x86_64
+#GNU bash, version 4.3.11(1)-release (x86_64-pc-linux-gnu)
+#curl 7.35.0 (x86_64-pc-linux-gnu) libcurl/7.35.0 OpenSSL/1.0.1f zlib/1.2.8 libidn/1.28 librtmp/2.3
+
+#A script which bootstraps a Jenkins installation using Jervis
+
+curl -sLo provision_jenkins.sh 'https://raw.githubusercontent.com/samrocketman/home/master/bin/provision_jenkins.sh'
+chmod 755 provision_jenkins.sh
+#download jenkins, start it up, and update the plugins
+./provision_jenkins.sh bootstrap
+#install plugins we want
+./provision_jenkins.sh install-plugins cloudbees-folder job-dsl view-job-filters embeddable-build-status
+#restart jenkins
+./provision_jenkins.sh restart
+./provision_jenkins.sh cli create-job _jervis_generator < jervis_config.xml
+echo 'Visit http://localhost:8080/'
