287e9219846df6b797c43a9cbd3f385751e8222e
Sam Gleske <sam.mxracer@gmail.com>
2015-06-07 07:51:59 -0400

    Update configure-primary-view.groovy

--- scripts/configure-primary-view.groovy
+++ scripts/configure-primary-view.groovy
@@ -1,5 +1,6 @@
 /*
     Created by Sam Gleske (https://github.com/samrocketman/)
+    Sets the primary view of Jenkins.
  */
 Jenkins.instance.setPrimaryView(Jenkins.instance.getView('Welcome'))
 println 'Set primary view to Welcome.'
