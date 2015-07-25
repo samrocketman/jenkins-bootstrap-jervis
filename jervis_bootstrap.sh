01cd21925e9b85a0ed306be5d763cd6b0c909ce8
Sam Gleske <sam.mxracer@gmail.com>
2015-07-25 19:35:40 -0400

    Conditional restart for configurations

--- jervis_bootstrap.sh
+++ jervis_bootstrap.sh
@@ -20,8 +20,10 @@ export jenkins_url="${jenkins_url:-http://mirrors.jenkins-ci.org/war/latest/jenk
 echo "Bootstrap Jenkins via script console (may take a while without output)"
 echo "NOTE: you could open a new terminal and tail -f console.log"
 curl --data-urlencode "script=$(<./scripts/bootstrap.groovy)" http://localhost:8080/scriptText
-#restart jenkins
-./scripts/provision_jenkins.sh restart
+#conditional restart jenkins
+if $(curl -s --data-urlencode "script=$(<scripts/console-needs-restart.groovy)" http://localhost:8080/scriptText); then
+  ./scripts/provision_jenkins.sh restart
+fi
 #wait for jenkins to become available
 ./scripts/provision_jenkins.sh download-file "http://localhost:8080/jnlpJars/jenkins-cli.jar"
 #create the first job, _jervis_generator.  This will use Job DSL scripts to generate other jobs.
